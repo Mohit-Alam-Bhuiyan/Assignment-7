5955e34589ac305965a3fc50dd77fbf8efb85730
Mohit Alam Bhuiyan <mohitcseedu@gmail.com>
2023-03-09 13:05:17 +0600

    repoted part working

--- js/app.js
+++ js/app.js
@@ -323,7 +323,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    Posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
